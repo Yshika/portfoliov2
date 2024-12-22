f22e25b1955b98dffd750d9847532ac7564612aa
Yshika Agarwal <yshika0581@gmail.com>
2024-12-22 23:58:26 +0530

    refactor navigation component and styles; add responsive design and data-driven menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Nav from './components/Nav';
 
 function App() {
   return (
     <>
       <Nav />
-      <About />
+      {/* <About /> */}
     </>
   );
 }
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,19 +1,32 @@
+import { useCallback } from 'react';
+import menuIcon from '../../assets/hamburger.png';
+import data from '../../data.json';
 import './style.scss';
 
 const Nav: React.FC = () => {
+  const handleTitleClick = useCallback(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href='#about'>About</a>
-        </li>
-        <li>
-          <a href='#projects'>Projects</a>
-        </li>
-        <li>
-          <a href='#contact'>Contact</a>
-        </li>
+    <nav className='nav'>
+      <a className='nav_title' onClick={handleTitleClick}>
+        {data.name}
+      </a>
+
+      <ul className='nav_list'>
+        {data.nav.map((item, index) => (
+          <li key={index}>
+            <a href={item?.key} rel='noreferrer'>
+              {item?.name}
+            </a>
+          </li>
+        ))}
       </ul>
+
+      <div className='nav_menu'>
+        <img src={menuIcon} alt='menu' />
+      </div>
     </nav>
   );
 };
