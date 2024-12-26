6adc8d298acc7530f66dded543eafe0de2dc5cc8
Yshika Agarwal <yshika0581@gmail.com>
2024-12-26 23:43:28 +0530

    add Experience component with HeadSection for improved headings

---
+++ src/components/Experience/index.tsx
@@ -0,0 +1,12 @@
+import HeadSection from '../common/HeadSection';
+import './style.scss';
+
+const Experience = () => {
+  return (
+    <div>
+      <HeadSection preHeading='Explore My' heading='Experience' />
+    </div>
+  );
+};
+
+export default Experience;
