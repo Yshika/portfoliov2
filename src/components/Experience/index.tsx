f8a8b26027cd9dad9fc3b815468c77dde2649e61
Yshika Agarwal <yshika0581@gmail.com>
2024-12-27 22:22:18 +0530

    add Experience component with dynamic skills display and responsive styles

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,11 +1,40 @@
 import HeadSection from '../common/HeadSection';
 import './style.scss';
+import data from '../../data.json';
 
 const Experience = () => {
   return (
-    <div>
+    <section id='experience' className='experience'>
       <HeadSection preHeading='Explore My' heading='Experience' />
-    </div>
+      <div className='box-container'>
+        {data?.experience?.map(
+          (
+            exp: {
+              heading: string;
+              skills: { [key: string]: string | undefined };
+            },
+            index: number
+          ) => (
+            <div className='box' key={index}>
+              <h1 className='sub-heading'>{exp?.heading}</h1>
+              <div className='skills-container'>
+                {Object.keys(exp?.skills)?.map((skill, index) => (
+                  <div className='skills' key={index}>
+                    <div className='checkmark'>
+                      <img src='/checkmark.png' alt='check' />
+                    </div>
+                    <div>
+                      <h3>{skill}</h3>
+                      <p>{exp?.skills?.[skill]}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </section>
   );
 };
 
